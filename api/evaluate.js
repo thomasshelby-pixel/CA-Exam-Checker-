30b251fcbfec936779732a49fa0a888400d04374
thomasshelby-pixel <wildpanther004@gmail.com>
2026-08-21 18:52:39 +0530

    Update evaluate.js

--- api/evaluate.js
+++ api/evaluate.js
@@ -1,13 +1,19 @@
-export default async function handler(req,res){
+export default async function handler(
+  req,
+  res
+) {
 
-  if(req.method!=="POST"){
+  if (req.method !== "POST") {
     return res.status(405).json({
-      error:"Method not allowed"
+      error:
+        "Method not allowed"
     });
   }
 
   return res.status(200).json({
-    success:true,
+    success:
+      true,
+
     message:
       "Evaluation is handled by /api/check."
   });
